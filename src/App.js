848e5a57fe4c0e88710f5e8c0a7c95943b8a835b
bb <whynot.byoul@gmail.com>
2023-04-13 07:45:26 +0900

    styled components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import styled from "styled-components";
+const Father = styled.div`
+  display: flex;
+`;
 function App() {
-  return null;
+  return (
+    <Father>
+      <div style={{ backgroundColor: "teal", width: 100, height: 100 }}></div>
+      <div style={{ backgroundColor: "tomato", width: 100, height: 100 }}></div>
+    </Father>
+  );
 }
 
 export default App;
