c01cfd70ef428f3efe2ca6001f30582ad3412954
bb <whynot.byoul@gmail.com>
2023-04-17 13:23:44 +0900

    optional props

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,8 @@ import Circle from "./Circle";
 function App() {
   return (
     <div>
-<<<<<<< HEAD
       <Circle bgColor="teal" borderColor="yellow" />
       <Circle bgColor="tomato" text="im here" />
-=======
-      <Circle />
->>>>>>> 2642d964883888017a1456ac6cd1a013ce0b68d2
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-<<<<<<< HEAD
-=======
-
->>>>>>> 2642d964883888017a1456ac6cd1a013ce0b68d2
 import App from "./App";
 
 const root = ReactDOM.createRoot(
