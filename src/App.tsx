8c10166b7873576140f3cdd882effef925706887
whynot-byoul <whynot@hubilon.com>
2023-04-17 17:31:07 +0900

    office

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
+import { log } from "console";
+import { useState } from "react";
 import styled from "styled-components";
-import Circle from "./Circle";
 
 function App() {
+  const [value, setValue] = useState("");
+  const onchange = (e: React.FormEvent<HTMLInputElement>) => {
+    console.log(e.currentTarget.value);
+    // react typescript target-> currentTarget 으로 쓴다.
+    const {
+      currentTarget: { value },
+    } = e;
+    setValue(value);
+  };
+  const onsubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(`hello ${value}`);
+  };
   return (
     <div>
-      <Circle bgColor="teal" borderColor="yellow" />
-      <Circle bgColor="tomato" text="im here" />
+      <form onSubmit={onsubmit}>
+        <input
+          type="text"
+          placeholder="username"
+          value={value}
+          onChange={onchange}
+        />
+        <button>Log in</button>
+      </form>
     </div>
   );
 }
