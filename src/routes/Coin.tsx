c3a8947babd3fe95a6ea5803cfea875bbad9bbe2
whynot-byoul <whynot@hubilon.com>
2023-04-20 09:40:18 +0900

    study

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,4 +1,15 @@
+import { useParams } from "react-router";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.accentColor};
+`;
+interface RouteParams {
+  coinId: string;
+}
 function Coin() {
-  return;
+  const { coinId } = useParams<RouteParams>();
+
+  return <Title>Coin : {coinId} </Title>;
 }
 export default Coin;
--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,7 @@
 import { DefaultTheme } from "styled-components";
 
 export const theme: DefaultTheme = {
-  bgColor: "white",
-  textColor: "black",
-  btnColor: "tomato",
+  bgColor: "#2d3436",
+  textColor: "#dfe6e9",
+  accentColor: "#00b894",
 };
