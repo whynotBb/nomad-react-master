11aeb7fed7f987f47d2f78879ad010580fb183ea
whynot-byoul <whynot@hubilon.com>
2023-04-20 18:05:51 +0900

    study

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,4 +1,79 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  padding: 0px 20px;
+`;
+const Header = styled.div`
+  height: 15vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const CoinsList = styled.ul``;
+const Coin = styled.li`
+  background-color: white;
+  color: ${(props) => props.theme.bgColor};
+  border-radius: 15px;
+  margin-bottom: 10px;
+  a {
+    padding: 20px;
+    transition: color 0.2s ease-in;
+    display: block;
+  }
+  &:hover {
+    a {
+      color: ${(props) => props.theme.accentColor};
+    }
+  }
+`;
+
+const Title = styled.h1`
+  font-size: 48px;
+  color: ${(props) => props.theme.accentColor};
+`;
+
+const coins = [
+  {
+    id: "btc-bitcoin",
+    name: "Bitcoin",
+    symbol: "BTC",
+    rank: 1,
+    is_new: false,
+    is_active: true,
+    type: "coin",
+  },
+  {
+    id: "eth-ethereum",
+    name: "Ethereum",
+    symbol: "ETH",
+    rank: 2,
+    is_new: false,
+    is_active: true,
+    type: "coin",
+  },
+  {
+    id: "hex-hex",
+    name: "HEX",
+    symbol: "HEX",
+    rank: 3,
+    is_new: false,
+    is_active: true,
+    type: "token",
+  },
+];
+
 function Coins() {
-  return <h1>Coin</h1>;
+  return (
+    <Container>
+      <Header>
+        <Title>Coin</Title>
+        <CoinsList>
+          {coins.map((coin) => (
+            <Coin key={coin.id}>{coin.name}</Coin>
+          ))}
+        </CoinsList>
+      </Header>
+    </Container>
+  );
 }
 export default Coins;
