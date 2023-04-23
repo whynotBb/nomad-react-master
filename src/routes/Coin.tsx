4ffd9cb1ff9613dfdc648835ac98d408b9329444
bb <whynot.byoul@gmail.com>
2023-04-24 07:42:50 +0900

    s

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useLocation, useParams } from "react-router";
 import styled from "styled-components";
 
@@ -29,12 +29,27 @@ function Coin() {
   const { coinId } = useParams<RouteParams>();
   const [loading, setLoding] = useState(true);
   const { state } = useLocation<RouteState>();
+  const [info, setInfo] = useState({});
+  const [priceInfo, setPriceInfo] = useState({});
   // Link 로 보내준 데이터 정보를 useLocation 을 통해 확인
-
+  useEffect(() => {
+    (async () => {
+      const infoData = await (
+        await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
+      ).json();
+      const priceData = await (
+        await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
+      ).json();
+      console.log(priceData);
+      setInfo(infoData);
+      setPriceInfo(priceData);
+    })();
+  }, []);
   return (
     <Container>
       <Header>
         <Title>{state?.name || "loading..."}</Title>
+        {/* state가 존재하면 name 을 가져오고 없으면 loading */}
       </Header>
       {loading ? <Loader>Loading...</Loader> : null}
     </Container>
