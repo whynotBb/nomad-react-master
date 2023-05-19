67146aa9afc1d3c67369abb1c78ce78b38a7dc49
whynot-byoul <whynot@hubilon.com>
2023-05-19 12:59:59 +0900

    done button

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,12 +1,42 @@
-import { IToDo } from "../atom";
+import { useSetRecoilState } from "recoil";
+import { IToDo, toDoState } from "../atom";
 
-function ToDo({ text }: IToDo) {
+function ToDo({ text, category, id }: IToDo) {
+  const setToDos = useSetRecoilState(toDoState);
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    console.log(event.currentTarget.name);
+    const {
+      currentTarget: { name },
+    } = event;
+    setToDos((oldToDos) => {
+      const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
+      const newToDo = { text, id, category: name as any };
+      console.log(targetIndex);
+      return [
+        ...oldToDos.slice(0, targetIndex),
+        newToDo,
+        ...oldToDos.slice(targetIndex + 1),
+      ];
+    });
+  };
   return (
     <li>
       <span>{text}</span>
-      <button>Doing</button>
-      <button>ToDo</button>
-      <button>Done</button>
+      {category !== "DOING" && (
+        <button name="DOING" onClick={onClick}>
+          Doing
+        </button>
+      )}
+      {category !== "TODO" && (
+        <button name="TODO" onClick={onClick}>
+          ToDo
+        </button>
+      )}
+      {category !== "DONE" && (
+        <button name="DONE" onClick={onClick}>
+          Done
+        </button>
+      )}
     </li>
   );
 }
